be5d0aaffe3872c78cd53594e1a7cddd4da96a0e
aboodetk <TK.Al-Tekreeti@dal.ca>
2017-10-02 09:41:36 -0700

    i modified the code i made 5 list list 1 have assult and id 2nd have assult and its id 3rd is robbery and its id 4th and 5 is for theifs and also counting

--- task2.py
+++ task2.py
@@ -1,32 +1,47 @@
 filee=open('file')
 a=[]
+a1=[]
 b=[]
 c=[]
 d=[]
+d1=[]
 def func():
  count=0
  count1=0
  count2=0
  count3=0
+ count4=0
+ count5=0
  for line in filee:
   for i in line.split():
    #print(i)
-   if 'ASSAULT' in i:
+   if 'ASSAULT' and '1430' in i:
+    #print()
     count+=1
     #print(count)
     #print(word)
     a.append(i)
- #print(a)
-   elif 'ROBBERY' in i:
+   if 'ASSAULT' and '1420' in i:
+    #print()
     count1+=1
-    b.append(i)
-   elif 'BREAK' in i:
+    #print(count)
+    #print(word)
+    a1.append(i)
+ #print(a)
+   elif 'ROBBERY' and '1610' in i:
     count2+=1
-    c.append(i)
-   elif 'THEFT' in i:
+    b.append(i)
+   elif 'BREAK' and '2120' in i:
     count3+=1
+    c.append(i)
+   elif 'THEFT' and '2135' in i:
+    count4+=1
     d.append(i)
- print('ASSULT',count,"ROBBERY",count1,"BREAK",count2,"THEDT",count3)
+   elif 'THEFT' and '2142' in i:
+    count5+=1
+    d1.append(i)
+
+  #print('ASSULT',count,"ROBBERY",count1,"BREAK",count2,"THEDT",count3)
 func()
 
 
