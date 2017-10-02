7fe39949fcb7f0319fb8f5ba3beee421be854a87
aboodetk <TK.Al-Tekreeti@dal.ca>
2017-10-02 08:35:01 -0700

    finding the Assult word and counting it

--- task2.py
+++ task2.py
@@ -1,6 +1,15 @@
 filee=open('file')
-
+a=[]
 def func():
+ count=0
  for i in filee:
-  print(i)
+  for word in i.split():
+  #if i=='ASSAULT':
+   if 'ASSAULT' in word:
+    count+=1
+    print(count)
+    a.append(word)
+  print(a)
+  #count=count+1 
+  #print(count)
 func()
