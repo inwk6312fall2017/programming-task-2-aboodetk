678b859ee2894768b450975898b48a3a9ad2c129
aboodetk <TK.Al-Tekreeti@dal.ca>
2017-10-02 08:54:42 -0700

    i have addede the crime type and count in a list like each type of crime is in seperate list

--- task2.py
+++ task2.py
@@ -1,15 +1,36 @@
 filee=open('file')
 a=[]
+b=[]
+c=[]
+d=[]
 def func():
  count=0
- for i in filee:
-  for word in i.split():
-  #if i=='ASSAULT':
-   if 'ASSAULT' in word:
+ count1=0
+ count2=0
+ count3=0
+ for line in filee:
+  for i in line.split():
+   #print(i)
+   if 'ASSAULT' in i:
     count+=1
-    print(count)
-    a.append(word)
-  print(a)
-  #count=count+1 
-  #print(count)
+    #print(count)
+    #print(word)
+    a.append(i)
+ #print(a)
+   elif 'ROBBERY' in i:
+    count1+=1
+    b.append(i)
+   elif 'BREAK' in i:
+    count2+=1
+    c.append(i)
+   elif 'THEFT' in i:
+    count3+=1
+    d.append(i)
+ print(a,count)
+ print(b,count1)
+ print(c,count2)
+ print(d,count3)
 func()
+
+
+
